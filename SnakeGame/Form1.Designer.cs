5eef3fc08e8ba8fe7865f93cc79bbc5d93c8434b
Halim Lais <halimlais@hotmail.com>
2018-06-08 20:12:52 +0100

    Game board and sprites
    variables and game board set up!

--- SnakeGame/Form1.Designer.cs
+++ SnakeGame/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SnakeGame
 {
-    partial class Form1
+    partial class frmSnake
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,74 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSnake));
+            this.picGameBoard = new System.Windows.Forms.PictureBox();
+            this.scrLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picGameBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGameBoard
+            // 
+            this.picGameBoard.Location = new System.Drawing.Point(50, 30);
+            this.picGameBoard.Name = "picGameBoard";
+            this.picGameBoard.Size = new System.Drawing.Size(420, 420);
+            this.picGameBoard.TabIndex = 0;
+            this.picGameBoard.TabStop = false;
+            // 
+            // scrLabel
+            // 
+            this.scrLabel.AutoSize = true;
+            this.scrLabel.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrLabel.Location = new System.Drawing.Point(45, 464);
+            this.scrLabel.Name = "scrLabel";
+            this.scrLabel.Size = new System.Drawing.Size(71, 30);
+            this.scrLabel.TabIndex = 1;
+            this.scrLabel.Text = "Score:";
+            this.scrLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "bonus1.png");
+            this.imgList.Images.SetKeyName(1, "bonus2.png");
+            this.imgList.Images.SetKeyName(2, "bonus3.png");
+            this.imgList.Images.SetKeyName(3, "bonus4.png");
+            this.imgList.Images.SetKeyName(4, "snake_body.png");
+            this.imgList.Images.SetKeyName(5, "snake_head.png");
+            this.imgList.Images.SetKeyName(6, "wall.png");
+            // 
+            // frmSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(537, 517);
+            this.Controls.Add(this.scrLabel);
+            this.Controls.Add(this.picGameBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "frmSnake";
+            this.Text = "SNAKE ";
+            this.Load += new System.EventHandler(this.frmSnake_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picGameBoard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picGameBoard;
+        private System.Windows.Forms.Label scrLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ImageList imgList;
     }
 }
 
--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace SnakeGame
 {
-    public partial class Form1 : Form
+    public partial class frmSnake : Form
     {
-        public Form1()
+        Random randonGen;
+        enum GameBoardFields { Free, Snake, Bonus };
+        enum Directions { Up, Down, Left, Right };
+
+
+        //to identify position of snake
+        struct SnakeCoordinates
+        {
+            public int x;
+            public int y;
+        }
+
+        //gameboard 2D dimenstional array
+
+        GameBoardFields[,] gameBoardField;
+        //coardinates of snake. Not 2D array, since its STUCT hold 2d coordinates 
+        SnakeCoordinates[] snakeXY;
+        int snakeLength;
+        Directions direction;
+
+        //to paint on the surface
+        Graphics graphics;
+
+
+
+       
+        public frmSnake()
         {
             InitializeComponent();
+            //initalise variable. . 11 area will be wall
+            gameBoardField = new GameBoardFields[11, 11];
+            //game is 10x10
+            snakeXY = new SnakeCoordinates[100];
+            randonGen = new Random();
+        }
+
+        private void frmSnake_Load(object sender, EventArgs e)
+        {
+            //perpare picturebox to hold images, creat a new bitmap
+            picGameBoard.Image = new Bitmap(420, 420);
+            graphics = Graphics.FromImage(picGameBoard.Image);
+
+            graphics.Clear(Color.White);
+
+            for (int i = 1; i < 11; i++)
+            {
+                //top and bottom walls. i*35. 35 is the pxl sprite
+                graphics.DrawImage(imgList.Images[6], i * 35, 0);
+                graphics.DrawImage(imgList.Images[6], i * 35, 385);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                //top and bottom walls. i*35. 35 is the pxl sprite
+                graphics.DrawImage(imgList.Images[6], 0, i*35);
+                graphics.DrawImage(imgList.Images[6], 385, i *35);
+            }
+
         }
     }
 }
